615ddc57f99d8e1015f02e8f95f89e2fb2ef9f93
jacobwojoski <jacobwojoski@gmail.com>
2026-01-26 13:15:59 -0500

    Refactor some names.

--- src/main/java/org/wojo/wojosToolbelt/plugin/WojosToolbeltPlugin.java
+++ src/main/java/org/wojo/wojosToolbelt/plugin/WojosToolbeltPlugin.java
@@ -4,6 +4,7 @@ import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import org.wojo.wojosToolbelt.commands.ExampleCommand;
+import org.wojo.wojosToolbelt.commands.WojosToolbeltCommandCollection;
 
 import javax.annotation.Nonnull;
 
@@ -11,11 +12,11 @@ import javax.annotation.Nonnull;
  * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
  * event listeners.
  */
-public class WojosToolbelt extends JavaPlugin {
+public class WojosToolbeltPlugin extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public WojosToolbelt(@Nonnull JavaPluginInit init) {
+    public WojosToolbeltPlugin(@Nonnull JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
     }
@@ -24,5 +25,7 @@ public class WojosToolbelt extends JavaPlugin {
     protected void setup() {
         LOGGER.atInfo().log("Setting up plugin " + this.getName());
         this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+
+        this.getCommandRegistry().registerCommand(new WojosToolbeltCommandCollection());
     }
 }
