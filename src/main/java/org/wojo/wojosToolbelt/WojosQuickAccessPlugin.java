2225f6eecfde8113fea8a180d0098a106e7f3d72
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-28 15:29:34 -0500

    Add WojosQuickAccessPlugin.java file
    Change WojosToolbelt to WojosQuickAccess

--- src/main/java/org/wojo/wojosToolbelt/WojosQuickAccessPlugin.java
+++ src/main/java/org/wojo/wojosToolbelt/WojosQuickAccessPlugin.java
@@ -15,29 +15,42 @@ import javax.annotation.Nonnull;
  * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
  * event listeners.
  */
-public class WojosToolbeltPlugin extends JavaPlugin {
-
+public class WojosQuickAccessPlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
-    private PacketFilter inboundWatcher;
+    private PacketFilter _inbound_filter;
 
     public WojosToolbeltPlugin(@Nonnull JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
     }
 
+    private void registerComponents(){
+    }
+    private void registerSystems(){
+    }
+    private void registerEvents(){
+    }
+    private void registerCommands(){
+        this.getCommandRegistry().registerCommand(new WojosToolbeltCommandCollection());
+    }
+    private void registerPacketAdapters(){
+        this._inbound_hotbar_filter = PacketAdapters.registerInbound(new QuickAccessPacketAdapter());
+    }
+    
     @Override
     protected void setup() {
         LOGGER.atInfo().log("Setting up plugin " + this.getName());
-        this.getCommandRegistry().registerCommand(new WojosToolbeltCommandCollection());
-
-        HotbarToolbeltPacketAdapter handler = new HotbarToolbeltPacketAdapter();
-        this.inboundWatcher = PacketAdapters.registerInbound(handler);
+        this.registerComponents();
+        this.registerSystems();
+        this.registerEvents();
+        this.registerCommands();
+        this.registerPacketAdapters();
     }
 
     @Override
     protected void shutdown() {
-        if (this.inboundWatcher != null) {
-            PacketAdapters.deregisterInbound(this.inboundWatcher);
+        if (this._inbound_hotbar_filter != null) {
+            PacketAdapters.deregisterInbound(this._inbound_hotbar_filter);
         }
     }
 }
