d5424f308f1277ac86883dacc0a63256c458eba2
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-28 11:45:09 -0500

    Update comments in QuickAccessItemPickupEvent.java
    Add comment about using LivingEntityInventoryChangeEvent.

--- src/main/java/org/wojo/wojosToolbelt/Events/QuickAccessItemPickupEvent.java
+++ src/main/java/org/wojo/wojosToolbelt/Events/QuickAccessItemPickupEvent.java
@@ -1,3 +1,5 @@
 // Handle when QuickAccess Item is picked up by a player.
 // - Create QuickAccessComponent bassed on item picked up & Add it to the player
 // - Enable Packet Filter for player
+
+// Prob need to use: LivingEntityInventoryChangeEvent
