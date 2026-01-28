9e1d50d1efad3f99264e9fe4cea7f681519e04d1
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-28 11:42:05 -0500

    Enhance comments in QuickAccessEntityTickingSystem
    Expanded comments to clarify system functionality and behavior.

--- src/main/java/org/wojo/wojosToolbelt/Systems/QuickAcessEntityTickingSystem.java
+++ src/main/java/org/wojo/wojosToolbelt/Systems/QuickAcessEntityTickingSystem.java
@@ -1,1 +1,7 @@
-// This system happens on every tick. It checks every player with a QuickAccess component and makes sure the item is stored in the needed hotbar slot
+// This system happens on every tick. 
+// It checks every player with a QuickAccess component 
+// - Makes sure a quickaccess item is stored in the needed hotbar slot
+// - If no item in inventory exists, remove component from player
+// - Check if item in hotbar matches saved value in compoenent. 
+//    If not: Update the component info
+//    else: do nothing
