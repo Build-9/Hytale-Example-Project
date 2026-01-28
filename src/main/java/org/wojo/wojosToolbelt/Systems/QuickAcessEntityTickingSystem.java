597d793fa57b43297082bef71a555916c0ea0c81
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-28 16:02:24 -0500

    Update QuickAcessEntityTickingSystem.java

--- src/main/java/org/wojo/wojosToolbelt/Systems/QuickAcessEntityTickingSystem.java
+++ src/main/java/org/wojo/wojosToolbelt/Systems/QuickAcessEntityTickingSystem.java
@@ -5,3 +5,19 @@
 // - Check if item in hotbar matches saved value in compoenent. 
 //    If not: Update the component info
 //    else: do nothing
+public class QuickAccessEntityTickingSystem extends DelayedEntitySystem<EntityStore> {
+  public HealthRegenSystem() {
+    super(1.0f); // Don't need super rapid updates (TODO: Update this value)
+  }
+  @Override
+  public void tick(float dt, int index, @Nonnull ArchetypeChunk<EntityStore> archetypeChunk,
+      @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
+      // Runs every 1 second per matching entity
+  }
+  
+  @Nonnull
+  @Override
+  public Query<EntityStore> getQuery() {
+    return Query.and(QuickAccessComponent.getComponentType());
+  }
+}
