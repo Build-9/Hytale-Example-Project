7bd73e63378887d817b62ea6037157ee72b48fd0
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-28 11:46:58 -0500

    Clarify comment on QuickAccessComponent creation
    Updated comment to clarify QuickAccessComponent creation.

--- src/main/java/org/wojo/wojosToolbelt/Events/QuickAccessItemPickupEvent.java
+++ src/main/java/org/wojo/wojosToolbelt/Events/QuickAccessItemPickupEvent.java
@@ -1,5 +1,5 @@
 // Handle when QuickAccess Item is picked up by a player.
-// - Create QuickAccessComponent bassed on item picked up & Add it to the player
+// - Create QuickAccessComponent bassed on item picked up (Copy from item?)  & Add it to the player
 // - Enable Packet Filter for player
 
 // Prob need to use: LivingEntityInventoryChangeEvent
