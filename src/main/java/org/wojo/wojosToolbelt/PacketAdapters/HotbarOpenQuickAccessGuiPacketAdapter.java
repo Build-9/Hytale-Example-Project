0ad0e3fd4cdbbca2bba3703f7f13e0027490ed77
jacobwojoski <jacobwojoski@gmail.com>
2026-01-28 22:03:34 -0500

    Add new Toolbelt Component to be named sling component

--- src/main/java/org/wojo/wojosToolbelt/PacketAdapters/HotbarOpenQuickAccessGuiPacketAdapter.java
+++ src/main/java/org/wojo/wojosToolbelt/PacketAdapters/HotbarOpenQuickAccessGuiPacketAdapter.java
@@ -17,7 +17,11 @@ import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 // Send Packet to player to tell them they are actually holding the original slected item not hotbar 9
 import com.hypixel.hytale.protocol.packets.inventory.SetActiveSlot;
 import com.hypixel.hytale.server.core.inventory.Inventory;
+import org.wojo.wojosToolbelt.Components.QuickAccessComponent;
 import org.wojo.wojosToolbelt.WojosQuickAccessPlugin;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 // Update the hotbar interaction to check if the player it swaping to the same item tey have equipped.
 // If they are then open UI.
@@ -27,12 +31,13 @@ public class HotbarOpenQuickAccessGuiPacketAdapter implements PlayerPacketFilter
     private static final int ABILITY_SLOT = 8;  // Slot index 8 = Key "9"
 
     static int counter = 0;
+    ConcurrentHashMap<PlayerRef, Boolean> quickAccessMap = new ConcurrentHashMap<>();
+
     // Returns boolean - "blockPacket"
     //    - True: Block Packet
     //    - False: Let Packet Through
     @Override
     public boolean test(PlayerRef playerRef, Packet packet) {
-
         // 290 == SyncInteractionCain packet ID
         if (packet.getId() != 290 ){
             return false;
@@ -44,19 +49,28 @@ public class HotbarOpenQuickAccessGuiPacketAdapter implements PlayerPacketFilter
         Ref<EntityStore> entityRef = playerRef.getReference();
         if (entityRef != null && entityRef.isValid()) {
 
-            // TODO: Add check if player has QucikAccessComponent
+            // TODO: Add check if player has QuickAccessComponent
             Store<EntityStore> store = entityRef.getStore();
-            if (store.getComponent(entityRef, WojosQuickAccessPlugin.get().getQuickAccessComponentType()) == null){
+            World world = store.getExternalData().getWorld();
+
+            world.execute(() -> {
+                QuickAccessComponent qaComp = store.getComponent(entityRef, WojosQuickAccessPlugin.get().getQuickAccessComponentType());
+                boolean hasQa = qaComp != null;
+                quickAccessMap.put(playerRef, hasQa);
+            }); // End threadsafty
+
+            if ( quickAccessMap.get(playerRef) == null || quickAccessMap.get(playerRef) == false){
                 // Try not to spam log
                 if (counter % 10 == 0) {
                     WojosQuickAccessPlugin.LOGGER.atInfo().log("Player Does not have a Quick AccessComponent so letting packets through");
                 }
                 counter++;
+
                 return false;
             }
 
             playerRef.sendMessage(Message.raw("Good Packet! : " + syncPacket.getId() + " | " + syncPacket.getClass().getName()));
-    
+
             // Check is user is trying to swap to item 9
             for (SyncInteractionChain chain : syncPacket.updates) {
                if ( (chain.interactionType == InteractionType.SwapFrom || chain.interactionType == InteractionType.SwapTo)
@@ -70,19 +84,19 @@ public class HotbarOpenQuickAccessGuiPacketAdapter implements PlayerPacketFilter
                     // Interacting with world comp required us to be threadsafe so update on world thread not network thread.
                     //    - This may not be needed as we dont actually set any world data.
                     //    - we just get data and send a packet to client to update selected item.
-                    World world = store.getExternalData().getWorld();
-                    world.execute(() -> {
-                        // Revert Selected Hotbar Item
-                        revertSelectedHotbarItem(chain.activeHotbarSlot, playerRef);
+
+                        world.execute(() -> {
+                            // Revert Selected Hotbar Item
+                            revertSelectedHotbarItem(chain.activeHotbarSlot, playerRef);
+                        });
 
                         // Open UI
                         openQuickAccessUI(playerRef);
-                    }); // end Threadsafty
-                    
+
                     // Block Packet as we don't want player to actually change to hotbar 9
-                    return true;
+                   return true;
                } // End chain type
-           } // End chain packet for loop
+            } // End chain packet for loop
         }// Bad entity Ref
 
         // Something went wrong or user wasnt pressing key 9, so dont block sync packet. 
