b9008826f00bb5d248b3cc1104da75f92eb0c11a
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-29 16:44:32 -0500

    Update getItemIdCommand.java

--- src/main/java/org/wojo/wojosToolbelt/Commands/ComponentCommands/getItemIdCommand.java
+++ src/main/java/org/wojo/wojosToolbelt/Commands/ComponentCommands/getItemIdCommand.java
@@ -1,1 +1,18 @@
-
+// Get a UUID for the held item
+public class getItemIdCommand extends AbstractPlayerCommand {
+  
+    // Constructor
+    public getItemIdCommand(){
+        super("WQA_GetItemId", "Get a unique UUID for the held itemstack");
+        addAliases("wqa_GII");
+    };
+  
+    @Override
+    protected void execute(@NonNullDecl CommandContext commandContext, @NonNullDecl Store<EntityStore> store, @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world) {
+        WojosQuickAccessPlugin.LOGGER.atInfo().log("Getting ID of held item.");
+        // TODO: Get held item
+        // TODO: Get Id of held item
+        // TODO: Create UUID if ones doesn't exist & Add to Item Stack
+        // TODO: Print UUID to terminal and log
+    }
+}
