085b73612bf00c4d139183687de50c561782dd43
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-01-30 09:41:10 -0500

    Update SwapWeaponHandler.java

--- src/main/java/org/wojo/wojosToolbelt/Handlers/SwapWeaponHandler.java
+++ src/main/java/org/wojo/wojosToolbelt/Handlers/SwapWeaponHandler.java
@@ -8,7 +8,7 @@ import java.util.function.Consumer;
 public class SwapWeaponHandler implements Consumer<SwapWeaponEvent> {
     @Override
     public void accept(SwapWeaponEvent event) {
-        WojosQuickAccessPlugin.LOGGER.atInfo().log("A player weapon Swap has just happened! We got the event!");
+        WojosQuickAccessPlugin.LOGGER.atInfo().log("A player weapon Swap has just happened! We got the event! User swapped from "+event.oldItemUUID+" to "+newItemUUID);
 
         // TODO: If we have penalties to a weapon swap they would happen here. (Like Take player stamina)
     }
