ff4b190ea30a5467d4ec1787df3c7d37d3752a39
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-02-02 15:09:29 -0500

    Update ConfigAccessConfig.java

--- src/main/java/org/wojo/wojosToolbelt/Config/ConfigAccessConfig.java
+++ src/main/java/org/wojo/wojosToolbelt/Config/ConfigAccessConfig.java
@@ -10,40 +10,64 @@ public class QuickAccessConfig {
     
     // Possible Item Tiers. Mythic is not craftable.
     public static final enum ITEM_TIER {
-        CRUDE,
-        COMMON,
-        RARE,
-        EPIC,
-        LEGENDARY,
-        MYTHIC,
-        CREATIVE
+        UNKONWN(0),
+        CRUDE(1),
+        COMMON(2),
+        RARE(3),
+        EPIC(4),
+        LEGENDARY(5),
+        MYTHIC(6),
+        CREATIVE(7);
+
+        private final int id;
+        ITEM_TIER(int id) {this.id = id;}
+        public int getId() {return id;}
+        public static ITEM_TIER fromId(int id) {
+            for (ITEM_TIER t : values()) {
+                if (t.id == id) {return t;}
+            }
+        }
     }
 
     public static final enum ITEM_TYPE {
-        TOOLBELT,
-        BUILDERS_POUCH,
-        WEAPON_SLING,
-        BANDOLIER,
-        QUIVER,
-        CREATIVE
+        UNKNOWN(0), 
+        TOOLBELT(1),
+        BUILDERS_POUCH(2),
+        WEAPON_SLING(3),
+        BANDOLIER(4),
+        QUIVER(5),
+        CREATIVE(6);
+
+        private final int id;
+        ITEM_TYPE(int id) {this.id = id;}
+        public int getId() {return id;}
+        public static ITEM_TIER fromId(int id) {
+            for (ITEM_TYPE t : values()) {
+                if (t.id == id) {return t;}
+            }
+        }
     }
     
     
     // Toolbelts can only Items with tool tag
-    static final Integer[] TOOLBELT_ARRAY = {3,3,3,3,3,3,3};
+    static final Integer[] TOOLBELT_ARRAY =         {0,3,3,3,3,3,3,3};
 
     // Builders pouch can hold any building block
-    static final Integer[] BUILDERS_POUCH_ARRAY = {3,3,3,3,3,3,3};
+    static final Integer[] BUILDERS_POUCH_ARRAY =   {0,3,3,3,3,3,3,3};
 
     // Slings can only hold weapons
-    static final Integer[] WEAPON_SLING_ARRAY = {3,3,3,3,3,3,3};
+    static final Integer[] WEAPON_SLING_ARRAY =     {0,3,3,3,3,3,3,3};
 
     // Bandoleers can only hold consumables (Food, Bombs, Potions, but cant hold arrows)
-    static final Integer[] BANDOLIER_ARRAY = {3,3,3,3,3,3,3};
+    static final Integer[] BANDOLIER_ARRAY =        {0,3,3,3,3,3,3,3};
     
     // Quivers can only hold arrows
-    static final Integer[] QUIVER_ARRAY = {3,3,3,3,3,3,3};
+    static final Integer[] QUIVER_ARRAY =           {0,3,3,3,3,3,3,3};
 
     // Creative can hold anything
-    static final Integer[] CREATIVE_ARRAY = {3,3,3,3,3,3,3}    
+    static final Integer[] CREATIVE_ARRAY =         {0,3,3,3,3,3,3,3};
+
+    // TODO: Add map to allow server owners to whitelist or blacklist specific Item ID's as specific types. 
+    //    This allows easy way to add Modded items as a type and allow items to be used in multiple quick access items. 
+    // ITEM_ACCESS_MAP<String, boolean[ITEM_TYPE.length()]> = new ConcurrentHashMap(String, new Array());
 }
