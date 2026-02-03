591f7252051159dc3f2f982cbe08685b250996d7
jacobwojoski <jacobwojoski@gmail.com>
2026-02-02 20:31:53 -0500

    Add new Toolbelt Component to be named sling component

--- src/main/java/org/wojo/wojosToolbelt/Handlers/SwapItemHandler.java
+++ src/main/java/org/wojo/wojosToolbelt/Handlers/SwapItemHandler.java
@@ -24,8 +24,14 @@ public class SwapItemHandler implements Consumer<SwapItemEvent> {
 
         Ref<EntityStore> player =  event.playerRef();
 
-        String originalItem = event.oldItemUUID();
-        String newItem = event.newItemUUID();
+        // TODO: Add getters for positions
+        int sourceInventory = event.sourceInventoryType();
+        short sourcePos = event.sourceInventoryPosition();
+
+        int destInventory = event.destInventoryType();
+        short destPos = event.destInventoryPosition();
+
+
 
         Store<EntityStore> store = player.getStore();
         QuickAccessComponent qaComp = store.getComponent(player, QuickAccessComponent.getComponentType());
@@ -37,30 +43,7 @@ public class SwapItemHandler implements Consumer<SwapItemEvent> {
             ItemContainer mainInventory = playerComp.getInventory().getStorage();
             ItemContainer hotbar = playerComp.getInventory().getHotbar();
 
-            int inventoryType = Inventory.STORAGE_SECTION_ID;
-            AtomicBoolean foundItem = new AtomicBoolean(false);
-            AtomicInteger foundPosition = new AtomicInteger();
-
-            // Only search main inventory for items to swap in & get needed postion info
-            mainInventory.forEach( (position, itemStack ) -> {
-                if (itemStack.getItemId().equals(newItem)){
-                    foundItem.set(true);
-                    foundPosition.set(position);
-                };
-            });
-
-
-            if (foundItem.get()){
-                int foundPositionInt = foundPosition.intValue();
-                short foundPositionShort = (short) foundPositionInt;
-
-                int targetPositionInt = qaComp.getSwapTargetLocation();
-                short targetPositionShort = (short) targetPositionInt;
-
-
-                mainInventory.swapItems(foundPositionShort, hotbar, targetPositionShort, Inventory.DEFAULT_HOTBAR_CAPACITY);
-            }
-
+            mainInventory.swapItems(sourcePos, hotbar, destPos, Inventory.DEFAULT_HOTBAR_CAPACITY);
 
         }
     }
