e2ae6e55a41f89ed965f1566e0a6a68daa62060d
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-02-03 12:13:54 -0500

    Log warning for too many displayed items
    Add warning log for exceeding displayed items limit

--- src/main/java/org/wojo/wojosToolbelt/ui/ToolSelectionGui.java
+++ src/main/java/org/wojo/wojosToolbelt/ui/ToolSelectionGui.java
@@ -44,6 +44,7 @@ public class ToolSelectionGui extends BasicCustomUIPage {
             QuickAccessComponent qa_comp = store.getComponent(ref, QuickAccessComponent.getComponentType());
             if (qa_comp != null){
                 this._num_displayed_items = qa_comp.getMaxTotalItems();
+                
             }
         }
     }
@@ -52,6 +53,9 @@ public class ToolSelectionGui extends BasicCustomUIPage {
     public void build(@Nonnull UICommandBuilder uiCommandBuilder) {
         // Path is relative to:
         //      src/main/resources/Common/UI/Custom
+        if(this._num_displayed_items > QuickAccessConfig.STORAGE_ITEM_GUIS[QuickAccessConfig.STORAGE_ITEM_GUIS.length - 1]){
+            WojosQuickAccessPlugin.LOGGER.atInfo().log("WARNING: Trying to display more items then we have a GUI for. Some items won't be selectable");
+        }
         uiCommandBuilder.append("Pages/ToolSelectionUI.ui");
         // TODO: Load different displays bassed on number of components?
     }
