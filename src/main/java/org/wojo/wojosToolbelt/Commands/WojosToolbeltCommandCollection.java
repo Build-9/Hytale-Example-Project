89e4c3c567528e427d8ad2ac348e17e771c37ccc
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-02-04 13:44:41 -0500

    Uncomment command documentation in WojosToolbeltCommandCollection

--- src/main/java/org/wojo/wojosToolbelt/Commands/WojosToolbeltCommandCollection.java
+++ src/main/java/org/wojo/wojosToolbelt/Commands/WojosToolbeltCommandCollection.java
@@ -3,22 +3,24 @@ package org.wojo.wojosToolbelt.Commands;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 import org.wojo.wojosToolbelt.Commands.Collections.ToolbeltSelectionGuiCommandCollection;
 // WojosQuickAccess                 - wqa
-//      Components                      - comp    (C)
-//          addToPlayer                     - add     (A)         // wqa comp add      <arg: None>
-//          removeFromPlayer                - rm      (R)         // wqa comp rm       <arg: None>
-//          printComponentData              - print   (P)         // wqa comp print    <arg: None>
-//      items                           - item    (I)
-//          addItemToComponent              -add      (A)         // wqa items add     <arg: uuid, pos(optional)>
-//          removeItemFromComponent         -rm       (R)         // wqa items rm      <arg: pos, uuid(optional)>
-//          swapItem                        -swap     (S)         // wqa items swap    <arg: [uuid(optional), pos(optional)](One Required), targetpos(optional)>
-//      gui                              -guis    (G)
-//          itemSelectionPage               -select   (ISE)      // wqa guis select    <arg: (o/c)>
-//          itemStoragePage                 -store    (IST)      // wqa guis store     <arg: (o/c)>
-//          helpPage                        -help      (H)       // wqa guis help      <arg: (o/c)>
-//      admin                            -admin   (A)
-//          settingsPage                    -settings  (S)       // wqa admin settings <arg: (o/c | true/false | 1/0)>
-//      user                             -user    (U)
-//          settingsPage                    -settings  (S)       // wqa user settings  <arg: (o/c)>
+//      components                      - comp  (C)
+//          addToPlayer                     - add      (A)         // wqa comp add      <arg: None>
+//          removeFromPlayer                - rm       (R)         // wqa comp rm       <arg: None>
+//          printComponentData              - print    (P)         // wqa comp print    <arg: None>
+//      items                           - item  (I)
+//          addItemToComponent              - add      (A)         // wqa items add     <arg: uuid, pos(optional)>
+//          removeItemFromComponent         - rm       (R)         // wqa items rm      <arg: pos, uuid(optional)>
+//          swapItem                        - swap     (S)         // wqa items swap    <arg: [uuid(optional), pos(optional)](One Required), targetpos(optional)>
+//      gui                              -guis  (G)
+//          itemSelectionPage               - select   (ISE)       // wqa guis select    <arg: (o/c)>
+//          itemStoragePage                 - store    (IST)       // wqa guis store     <arg: (o/c)>
+//          helpPage                        - help     (H)         // wqa guis help      <arg: (o/c)>
+//      admin                            -admin (A)
+//          settingsPage                    - settings (S)         // wqa admin settings <arg: (o/c | true/false | 1/0)>
+//      user                             -user  (U)
+//          settingsPage                    - settings (S)         // wqa user settings  <arg: (o/c)>
+//      help                             -help  (H)
+//          guide                           - guide    (G)         // wqa help guide <arg: none> (calls `wqa guis help o` to open help gui)
 
 public class WojosToolbeltCommandCollection extends AbstractCommandCollection {
     public WojosToolbeltCommandCollection() {
