bf3b605b7792a2944204ed24fff9937b23e0decf
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-02-04 13:48:18 -0500

    Create ItemStorageGui for item management
    Add ItemStorageGui for item selection and updates

---
+++ src/main/java/org/wojo/wojosToolbelt/ui/ItemStorageGui.java
@@ -0,0 +1,4 @@
+// UI page to pick what each button corresponds to. 
+// Get list of items from players hotbar & inventory that can be used and make dropdown list options
+// Allow user to select a dropdown to update item ID. 
+// Pick confirm to update players comp with new item array
