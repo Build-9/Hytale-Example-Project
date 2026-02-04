560ce987de61579c2c2901e11f9a190fb5507931
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-02-04 12:59:58 -0500

    Update command layout
    Create comment for how commands will be changed

--- src/main/java/org/wojo/wojosToolbelt/Commands/WojosToolbeltCommandCollection.java
+++ src/main/java/org/wojo/wojosToolbelt/Commands/WojosToolbeltCommandCollection.java
@@ -2,18 +2,26 @@ package org.wojo.wojosToolbelt.Commands;
 
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 import org.wojo.wojosToolbelt.Commands.Collections.ToolbeltSelectionGuiCommandCollection;
-// WojosQuickAccess                 -wqa
-//      Components                      -c
-//          addItemToPlayer                 -a
-//          removeItemFromPlayer            -r
-//          printComponentData              -p
-//      items - i                       -i
-//          addItemToComponent              -a
-//          removeItemFromComponent         -r
-//          swapItem                        -s
-//      gui - g                         -g
-//          openItemSelection               -o
-//          closeItemSelection              -c
+// WojosQuickAccess                 - wqa
+//      Components                      - comp   (C)
+//          addToPlayer                     - add     (A)         // wqa comp add
+//          removeFromPlayer                - rm      (R)         // wqa comp rm
+//          printComponentData              - print   (P)         // wqa comp print
+//      items                            -item    (I)
+//          addItemToComponent              -add      (A)         // wqa items add 
+//          removeItemFromComponent         -rm       (R)         // wqa items rm
+//          swapItem                        -swap     (S)         // wqa items swap
+//      gui                              -guis    (G)
+//          openItemSelectionPage           -ois       (OIS)      // wqa guis ois
+//          closeItemSelectionPage          -cis       (CIS)      // wqa guis cis
+//          openItemPlacementPage           -oip       (OIP)      // wqa guis oip
+//          closeItemPlacementPage          -cip       (CIP)      // wqa guis cip
+//      admin
+//          openSettingsPage                -osp       (OSP)      // wqa admin osp
+//          closeSettingsPage               -csp       (CSP)      // wqa admin csp
+//      user
+//          openHelpPage                    -ohp       (OHP)      // wqa user ohp
+//          closeHelpPage                   -chp       (CHP)      // wqa user chp
 
 public class WojosToolbeltCommandCollection extends AbstractCommandCollection {
     public WojosToolbeltCommandCollection() {
