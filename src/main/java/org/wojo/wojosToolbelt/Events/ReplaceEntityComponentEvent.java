155dc0f8b75f187107fe344008139eeb5bcf0411
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-02-05 16:04:25 -0500

    Rename ReplaceComponentEvent to ReplaceEntityComponentEvent

---
+++ src/main/java/org/wojo/wojosToolbelt/Events/ReplaceEntityComponentEvent.java
@@ -0,0 +1,25 @@
+package org.wojo.wojosToolbelt.Events;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.event.IEvent;
+import com.hypixel.hytale.event.IEventDispatcher;
+import com.hypixel.hytale.server.core.HytaleServer;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+public record ReplaceEntityComponentEvent(
+        Ref<EntityStore> entityRef,
+        QuickAccessComponent newComponent,
+) implements IEvent<Void> {
+    public static void dispatch(Ref<EntityStore> entityRef, QuickAccessComponent newComponent) {
+        IEventDispatcher<ReplaceEntityComponentEvent, ReplaceEntityComponentEvent> dispatcher =
+                HytaleServer.get().getEventBus().dispatchFor(ReplaceEntityComponentEvent.class);
+
+        if (dispatcher.hasListener()) {
+            dispatcher.dispatch(
+                    new ReplaceEntityComponentEvent(entityRef, newComponent)
+            );
+        }
+    }
+}
