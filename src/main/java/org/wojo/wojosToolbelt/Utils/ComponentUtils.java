58d9a30f13dd7374155a849ac0c0671a0f229d99
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-03-02 13:56:01 -0500

    Update ComponentUtils.java

--- src/main/java/org/wojo/wojosToolbelt/Utils/ComponentUtils.java
+++ src/main/java/org/wojo/wojosToolbelt/Utils/ComponentUtils.java
@@ -3,14 +3,51 @@ class ComponentUtils {
       // Update Quick Access Component Arrays from item Data
   }
 
+  public static boolean hasQuickAccessComponent(ItemStack itemStack) {
+    QuickAccessComponent comp = itemStack.getFromMetadataOrNull(QuickAccessConfig.QUICK_ACCESS_COMPONENT_ID, QuickAccessComponent.CODEC);
+    if (comp != null) {
+      return true;
+    }
+    return false;
+  }
+  
   // Get a quickAccessComponent from an item if it exists, else get null
-  private QuickAccessComponent getItemsQuickAccessComponent (ItemStack itemStack) {
+  public static QuickAccessComponent getItemsQuickAccessComponent (ItemStack itemStack) {
         return itemStack.getFromMetadataOrNull(QuickAccessConfig.QUICK_ACCESS_COMPONENT_ID, QuickAccessComponent.CODEC);
   }
 
   // Get array of items in a container if item has container field, else get null. 
-  private ItemStack[] getContainerComponentItems(ItemStack itemStack) {
+  public static ItemStack[] getContainerComponentItems(ItemStack itemStack) {
         BsonDocument containerBSON = itemStack.getFromMetadataOrNull(ItemStackItemContainer.CONTAINER_CODEC);
         return ItemStackItemContainer.ITEMS_CODEC.getOrNull(containerBSON, new ExtraInfo());
   }
+
+  // Get array of item names from an array of item stacks
+  public static String[] getItemNamesArray(ItemStack[] items){
+    String itemNames = new items[items.length()];
+    for (int i = 0; i < items.length; i++) {
+      if (items[i] != null) {
+        itemIds[i] = items[i].getItemName();
+      }else{
+        itemIds[i] = "null";
+      }
+    }
+    return itemNames;
+  }
+
+  // Get array of item ID's from an array of item Stacks
+  public static String[] getItemIdArray(ItemStack[] items){
+    String itemIds = new items[items.length()];
+    for (int i = 0; i < items.length; i++) {
+      if (items[i] != null) {
+        itemIds[i] = items[i].getId();
+      }else{
+        itemIds[i] = "null";
+      }
+    }
+    return itemIds;
+  }
+
+  
+  
 }
