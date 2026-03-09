e499e94918edd413134e52eaf39ac678172f01b6
JacobWojo <jwojciechowski@idtus.com>
2026-03-09 16:01:35 -0400

    prep app actual commands I need to get working

--- src/main/java/org/wojo/wojosToolbelt/Commands/WojosQuickAccessCommandCollection.java
+++ src/main/java/org/wojo/wojosToolbelt/Commands/WojosQuickAccessCommandCollection.java
@@ -4,9 +4,9 @@ import com.hypixel.hytale.server.core.command.system.basecommands.AbstractComman
 
 // WojosQuickAccess                 - wqa
 //      component                       - comp  (C)
-//          addToItem                       - add      (A)         // wqa comp add      <arg: invId, invPos> (Add Quick Access Component to item in inventory)
-//          removeFromItem                  - rm       (R)         // wqa comp rm       <arg: invID, invPos> (Remove Quick Access Component from item in inventory)
-//          printComponentData              - print    (P)         // wqa comp print    <arg: invID, invPos> (Print Quick Access Component info from item in inventory)
+//          playerSettings                  - player   (PS)        // wqa comp player   <arg: enable, equipPos, targetPos> (Update player comp with new settings)
+//          itemSettings                    - item     (IS)        // wqa comp item     <arg: tier, contSize, type, size, gui> (Update item QA comp settings)
+//          print                           - print    (P)         // wqa comp print    <arg: invID, invPos> (Print player comp data)
 //      item                            - item  (I)
 //          swap                            - swap     (S)         // wqa item swap     <arg: qaInvId, qaInvPos, qaItemPos> (Swap Hotabar item with Item Stack in Quick Access)
 //          print                           - print    (P)         // wqa item print    <arg: invID (Default: Hotbar=-1) + invPos (Default: 0)> (print item container data & Quick access comp data
@@ -15,10 +15,6 @@ import com.hypixel.hytale.server.core.command.system.basecommands.AbstractComman
 //          itemStoragePage                 - store    (STO)       // wqa guis store     <arg: qaInvId, qaInvPos, (o/c)> (Open or close item storage page tied to set Quick Access Item)
 //          settingsPage                    - settings (SET)       // wqa guis settings  <arg: qaInvId, qaInvPos, (o/c)> (Open or close item settings page tied to quick access item)
 //          helpPage                        - help     (HEL)       // wqa guis help      <arg: (o/c)>                    (Open or close global help page)
-//      user                             -user  (U)
-//          settingsPage                    - settings (S)         // wqa user settings  <arg: qaInvId, qaInvPos, (o/c)> (Open or close user settings page)
-//      help                             -help  (H)
-//          guide                           - guide    (G)         // wqa help guide <arg: none> (Calls: wqa guis help)
 
 public class WojosQuickAccessCommandCollection extends AbstractCommandCollection {
     public WojosQuickAccessCommandCollection() {
