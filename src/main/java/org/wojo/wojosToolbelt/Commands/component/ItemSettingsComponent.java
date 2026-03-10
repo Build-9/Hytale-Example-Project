8e55e77e5d94ae4315137a698ba84d4b819fc556
JacobWojo <jwojciechowski@idtus.com>
2026-03-10 16:27:42 -0400

    create new commands

---
+++ src/main/java/org/wojo/wojosToolbelt/Commands/component/ItemSettingsComponent.java
@@ -0,0 +1,44 @@
+package org.wojo.wojosToolbelt.Commands.component;
+
+// wqa comp item     <arg: tier, contSize, type, qaSize, gui> (Update held items quick access settings)
+public class ItemSettingsCommand extends AbstractPlayerCommand {
+    private final DefaultArg<Integer> tier;     // Item Tier (Uncommon, Common, Rare, Epic...)
+    private final DefaultArg<Integer> contSize; // Container Size (Size of storage for items)
+    private final DefaultArg<Integer> type;     // Item Type (Unrestricted, Toolbelt, Sling, ...)
+    private final DefaultArg<Integer> qaSize;   // Quick Swap Size (Number of enabled buttons on UI)
+    private final DefaultArg<String>  gui;      // GUI String
+
+    PlayerSettingsCommand() {
+        super("itemSettings", "Configure the players held Quick-Access-Item");
+        addAliases("item","IS");
+
+        this.tier = this.withDefaultArg(
+            "tier", "Set the item tier [UNKNOWN:0|COMMON:1|UNCOMMON:2|RARE:3|EPIC:4|LEGENDARY:5|MYTHIC:6|CREATIVE:7]",
+            ArgTypes.INTEGER,
+            1, "COMMON"
+        );
+
+        this.contSize = this.withDefaultArg(
+            "cont-size", "Set the items container size.",
+            ArgTypes.INTEGER,
+            10, "Ten items total."
+        );
+
+        this.type = this.withDefaultArg(
+            "type", "Set the type of the quick access item. [UNKNOWN:0|TOOLBELT:1|BUILDERS_POUCH:2|WEAPON_SLING:3|BANDOLIER:4|QUIVER:5|UNRESTRICTED:6|CUSTOM:7]",
+            ArgTypes.INTEGER,
+            6, "Unrestricted"
+        );
+
+        this.qaSize = this.withDefaultArg(
+            "qa-size", "Number of enabled quick access buttons",
+            ArgTypes.INTEGER,
+            2, "COMMON's default number of enabled items is 2 buttons."
+        );
+
+        this.gui = this.withDefaultArg(
+            "gui", "Path to UI To use. If player wants to use UI that has"
+        );
+    }
+}
+
---
+++ src/main/java/org/wojo/wojosToolbelt/Commands/component/PlayerSettingsCommand.java
@@ -0,0 +1,47 @@
+package org.wojo.wojosToolbelt.Commands.component;
+
+// wqa comp player   <arg: enable, equipPos, targetPos> (Update players quick access comp with new settings)
+public class PlayerSettingsCommand extends AbstractPlayerCommand {
+    private final DefaultArg<boolean> enable;
+    private final DefaultArg<Integer> equipPos;
+    private final DefaultArg<Integer> targetPos;
+
+    PlayerSettingsCommand() 
+    {
+        super("playerSettings","Configure the players Quick-Access-Component");
+        addAliases("player","PS");
+
+        this.enable = this.withDefaultArg(
+            "enable", "Enable or disable the players QuickAccessComponent. This enables the hotbar button", 
+            ArgTypes.BOOLEAN,
+            false, "Hotbar button to open gui is disabled (false) by default."
+        );
+
+        this.equipPos = this.withDefaultArg(
+            "equip-pos", "The position that the quick-access-item needs to be placed in and the button used to open the Quick Swap radial menu",
+            ArgTypes.INTEGER,
+            8, "Hotbar position 8 (Button #9)"
+        );
+
+        this.targetPos = this.withDefaultArg(
+            "target-pos", "Hotbar location that Quick Access items get swaped into. (-1 swaps items to active hotbar slot)",
+            ArgTypes.INTEGER,
+            0, "Hotbar position 0 (Button #1)."
+        );
+    }
+
+    @Override
+    protected void execute(
+        @NonNullDecl CommandContext commandContext, @NonNullDecl Store<EntityStore> store, 
+        @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, 
+        @NonNullDecl World world) 
+    {
+        boolean enabled = commandContext.get(PlayerSettingsCommand.enable);
+        Integer equipped = commandContext.get(PlayerSettingsCommand.equipPos);
+        Integer target = commandContext.get(PlayerSettingsCommand.targetPos);
+
+        QuickAccessPlayerComponent newQuickAccessPlayerComponent = new QuickAccessPlayerComponent(enabled, equipped, target);
+
+        store.setComponent(ref, QuickAccessPlayerComponent.getComponentType(), newQuickAccessPlayerComponent);
+    }
+}
