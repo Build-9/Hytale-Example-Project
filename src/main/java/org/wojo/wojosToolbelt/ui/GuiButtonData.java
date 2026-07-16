2b5ef7cc98f6ae0117f479ae91020db8056d77df
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2026-07-16 11:15:49 -0400

    Enhance ButtonData with highlight and press images
    Added fields for button highlight and press images to support new button styles.

--- src/main/java/org/wojo/wojosToolbelt/ui/GuiButtonData.java
+++ src/main/java/org/wojo/wojosToolbelt/ui/GuiButtonData.java
@@ -15,8 +15,10 @@ public static class GuiButtonData {
         public String buttonStyle = "";
         public String buttonHtmlId = "";
         public String iconHtmlId = "";
+        public String buttonHighlightImage = "";  // Background image to show when hovering over Icon
+        public String buttonPressImage = "";      // Background image to show when pressing the button
 
-        public ButtonData (Message msg, String text, String tooltip, String icon, String is_disabled, String style, String btn_html_id, String icn_html_id){
+        public ButtonData (Message msg, String text, String tooltip, String icon, String is_disabled, String style, String btn_html_id, String icn_html_id, String btn_highlight_img, String btn_press_img){
             this.buttonMsg = msg;
             this.buttonText = "";
             this.tooltipText = "";
@@ -26,6 +28,10 @@ public static class GuiButtonData {
             this.buttonStyle = style;
             this.buttonHtmlId = btn_html_id;
             this.iconHtmlId = icn_html_id;
+            // Used for radials as they were just different static images 
+            // (Can't make custom button shapes or handle mouse position to use code for UI so use buttons over a BG img)
+            this.buttonHighlightImage = btn_highlight_img;
+            this.buttonPressImage = btn_press_img;
         }
 
         public String getDebugString() {
@@ -43,6 +49,8 @@ public static class GuiButtonData {
             output += " - style: "+buttonStyle+"\n";
             output += " - btnHtmlId: "+buttonHtmlId+"\n";
             output += " - iconHtmlId: "+iconHtmlId+"\n";
+            output += " - highlightImg: "+buttonHighlightImage+"\n";
+            output += " - pressImg: "+buttonPressImage+"\n";
             return output;
         }
     }
