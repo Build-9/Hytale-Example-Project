20f6aa9e42e3d4866d5bf364fcc9befae69f2586
jacobwojoski <jacobwojoski@gmail.com>
2026-07-21 22:39:41 -0400

    Get Radials working

--- src/main/java/org/wojo/wojosToolbelt/ui/Radials/RadialGui4.java
+++ src/main/java/org/wojo/wojosToolbelt/ui/Radials/RadialGui4.java
@@ -28,7 +28,7 @@ public class RadialGui4 extends GenericRadialSelectionUi {
             String style = "";
             String htmlBtnId = "#QuickAccessButton"+String.valueOf(i);
             String htmlIconId = "#QuickAccessButton"+String.valueOf(i)+"Img";
-            String hoverImg = "";
+            String hoverImg = "QuickAccessRadialMenuDrawing_Four_"+String.valueOf(i)+".png";
             String pressImg = "";
             
             GuiButtonData btnData = new GuiButtonData(
@@ -38,6 +38,7 @@ public class RadialGui4 extends GenericRadialSelectionUi {
             
             this._quickAccessButtons.addLast(btnData);
         }
+        this._equipedItemButton.buttonHighlightImage = "../Radials/Images/Two/QuickAccessRadialMenuDrawing_Four_Equipped.png";
     }
 
     public RadialGui4(@NonNullDecl PlayerRef player_ref, Store<EntityStore> store, ItemStack quick_access_item, Integer quick_access_item_hotbar_position) {
